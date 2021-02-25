9ef0b224e101c0167f9e85e22f537346ad985227
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2021-02-25 20:43:15 +0000

    Update version.lua

--- qb-gangs/server/version.lua
+++ qb-gangs/server/version.lua
@@ -4,18 +4,18 @@ Citizen.CreateThread(
         if vRaw then
             local v = json.decode(vRaw)
             PerformHttpRequest(
-                "https://raw.githubusercontent.com/Mojito-Fivem/qb-gangs/main/version.json",
+                "https://raw.githubusercontent.com/Mojito-Fivem/qb-gangs/main/qb-gangs/version.json",
                 function(code, res, headers)
                     if code == 200 then
                         local rv = json.decode(res)
                         if rv.version ~= v.version then
                             print(
                                 ([[^1
-									-------------------------------------------------------
-									qb-gangs
-									UPDATE: %s AVAILABLE
-									CHANGELOG: %s
-									-------------------------------------------------------
+-----------------------------------------------
+	qb-gangs
+	UPDATE: %s AVAILABLE
+	CHANGELOG: %s
+-----------------------------------------------
 									^0]]):format(
                                     rv.version,
                                     rv.changelog
