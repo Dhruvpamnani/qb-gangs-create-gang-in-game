09ba51d7c758fd615c744d823a6d3c6ba0555e49
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2021-02-25 12:06:22 -0800

    Add version controll system

---
+++ qb-gangs/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+version '2.1'
+
+author 'Mojito-Fivem'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua',
+	'client/gui.lua',
+}
+
+server_scripts {
+	'server/version.lua'
+}
+
+file 'version.json'
