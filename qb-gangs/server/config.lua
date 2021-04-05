aa013fbb44439981da41e064e4af1bf5d36a0d53
Mojito-Fivem <66041893+Mojito-Fivem@users.noreply.github.com>
2021-04-05 22:35:54 +0100

    Create config.lua

---
+++ qb-gangs/server/config.lua
@@ -0,0 +1,19 @@
+Config = {
+	["GangLeaders"] = {
+		["ballas"] = {
+			"ORJ52463" -- Add Citizen IDs for gang leaders to add and remove people
+		},
+		["marabunta"] = {
+
+		},
+		["vagos"] = {
+
+		},
+		["families"] = {
+			
+		},
+		["lost"] = {
+
+		}
+	}
+}
