1da1de5a5d733edd1f856840945b361533aadd66
Mojito-Fivem <liamdormon04@gmail.com>
2021-08-30 19:17:56 +0100

    fix(territories): Being inside polyzone before loading in errors out

--- client/territories.lua
+++ client/territories.lua
@@ -33,56 +33,58 @@ end)
 Citizen.CreateThread(function()
     while true do 
         Citizen.Wait(500)
-        if PlayerGang.name ~= "none" or PlayerJob.name == "police" then
-            local PlayerPed = PlayerPedId()
-            local pedCoords = GetEntityCoords(PlayerPed)
+        if isLoggedIn then
+            if PlayerGang.name ~= "none" or PlayerJob.name == "police" then
+                local PlayerPed = PlayerPedId()
+                local pedCoords = GetEntityCoords(PlayerPed)
 
-            for k, zone in pairs(Territories) do
-                if Territories[k].zone:isPointInside(pedCoords) then
-                    insidePoint = true
-                    activeZone = Territories[k].id
-                    
-                    TriggerEvent("QBCore:Notify", "You have entered a gang territory", "success")
+                for k, zone in pairs(Territories) do
+                    if Territories[k].zone:isPointInside(pedCoords) then
+                        insidePoint = true
+                        activeZone = Territories[k].id
+                        
+                        TriggerEvent("QBCore:Notify", "You have entered a gang territory", "success")
 
-                    -- Whilst inside the zone we send a server event for the server sided calculations
-                    while insidePoint == true do
-                        TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, true)
+                        -- Whilst inside the zone we send a server event for the server sided calculations
+                        while insidePoint == true do
+                            TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, true)
 
-                        -- We fetch a callback for the most reason status of the zone and send it to the NUI
-                        QBCore.Functions.TriggerCallback("qb-gangs:server:getstatus", function(status, gang, score)
-                            SendNUIMessage({
-                                action = "showgang",
-                                data = {
-                                    status = status,
-                                    winner = gang,
-                                    score = score
-                                },
-                                gang = PlayerGang.label ~= "none" and PlayerGang.label or "Police",
-                                max = Zones["Config"].minScore
-                            })
-                        end, activeZone)
+                            -- We fetch a callback for the most reason status of the zone and send it to the NUI
+                            QBCore.Functions.TriggerCallback("qb-gangs:server:getstatus", function(status, gang, score)
+                                SendNUIMessage({
+                                    action = "showgang",
+                                    data = {
+                                        status = status,
+                                        winner = gang,
+                                        score = score
+                                    },
+                                    gang = PlayerGang.label ~= "none" and PlayerGang.label or "Police",
+                                    max = Zones["Config"].minScore
+                                })
+                            end, activeZone)
 
-                        if not Territories[k].zone:isPointInside(GetEntityCoords(PlayerPed)) then
-                            TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, false)
+                            if not Territories[k].zone:isPointInside(GetEntityCoords(PlayerPed)) then
+                                TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, false)
 
-                            insidePoint = false
-                            activeZone = nil
+                                insidePoint = false
+                                activeZone = nil
 
-                            QBCore.Functions.Notify("You have left a gang territory", "error")
+                                QBCore.Functions.Notify("You have left a gang territory", "error")
+
+                                Citizen.Wait(1000)
+
+                                SendNUIMessage({
+                                    action = "hide"
+                                })
+                            end
 
                             Citizen.Wait(1000)
-
-                            SendNUIMessage({
-                                action = "hide"
-                            })
                         end
-
-                        Citizen.Wait(1000)
                     end
                 end
+            else
+                Citizen.Wait(2000)
             end
-        else
-            Citizen.Wait(2000)
         end
     end
 end)
